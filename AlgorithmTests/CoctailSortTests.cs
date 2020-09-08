f8c339cd9f7bfe4179e2caf8dbd5e72702a9c038
Yersen <44286140+Yersen@users.noreply.github.com>
2020-09-08 19:56:59 +0600

    Реализована шейкерная сортировка

--- AlgorithmTests/CoctailSortTests.cs
+++ AlgorithmTests/CoctailSortTests.cs
@@ -9,28 +9,28 @@ using System.Threading.Tasks;
 namespace Algorithm.Tests
 {
     [TestClass()]
-    public class BubbleSortTests
+    public class CoctailSortTests
     {
         [TestMethod()]
         public void SortTest()
         {
             //arrange
-            var bubble = new BubbleSort<int>();
+            var coctail = new CoctailSort<int>();
             var rnd = new Random();
             var items = new List<int>();
-            for (int i = 0; i <10000; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 items.Add(rnd.Next(0, 100));
             }
-            bubble.Items.AddRange(items);
-            items.Sort();
+            coctail.Items.AddRange(items);
+            var sorted = items.OrderBy(x => x).ToArray();
             //act
-            bubble.Sort();
+            coctail.Sort();
 
             //assert
             for (int i = 0; i < items.Count; i++)
             {
-                Assert.AreEqual(items[i], bubble.Items[i]);
+                Assert.AreEqual(sorted[i], coctail.Items[i]);
             }
 
         }
